5bf901c19fa7b024a68d8c64b3ed13adbfafabdb
Ai-Dono <nchretiennot@protonmail.com>
2025-01-19 17:01:04 +0100

    setup test

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/sh
+
+# Set the necessary variables
+RESOURCE_GROUP="housing_prices"
+RESOURCE_PROVIDER="Microsoft.MachineLearning"
+REGION="francecentral"
+WORKSPACE_NAME="mlw-housing_prices"
+COMPUTE_INSTANCE="ci"
+COMPUTE_CLUSTER="aml-cluster"
+
+# Register the Azure Machine Learning resource provider in the subscription
+echo "Register the Machine Learning resource provider:"
+az provider register --namespace $RESOURCE_PROVIDER
+
+# Create the resource group and workspace and set to default
+echo "Create a resource group and set as default:"
+az group create --name $RESOURCE_GROUP --location $REGION
+az configure --defaults group=$RESOURCE_GROUP
+
+echo "Create an Azure Machine Learning workspace:"
+az ml workspace create --name $WORKSPACE_NAME 
+az configure --defaults workspace=$WORKSPACE_NAME 
+
+# Create compute instance
+echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
+az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_DS11_V2 --type ComputeInstance 
+
+# Create compute cluster
+echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
